b12f0718a12aaa5bdcafbdfae14c020de6b386be
OscarClemente <oscar.clemente@gmail.com>
2023-03-23 20:24:46 +0100

    feat: Add vim-test and gitignore

--- lua/hassio/packer.lua
+++ lua/hassio/packer.lua
@@ -39,4 +39,5 @@ return require('packer').startup(function(use)
 		  {'rafamadriz/friendly-snippets'}, -- Optional
 	  }
   }
+  use 'vim-test/vim-test'
 end)
--- lua/hassio/remap.lua
+++ lua/hassio/remap.lua
@@ -1,2 +1,19 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- vim-test
+vim.keymap.set("n", "<leader>tn", function()
+    vim.cmd("TestNearest")
+end)
+vim.keymap.set("n", "<leader>tf", function()
+    vim.cmd("TestFile")
+end)
+vim.keymap.set("n", "<leader>ts", function()
+    vim.cmd("TestSuite")
+end)
+vim.keymap.set("n", "<leader>tl", function()
+    vim.cmd("TestLast")
+end)
+vim.keymap.set("n", "<leader>tv", function()
+    vim.cmd("TestVisit")
+end)
