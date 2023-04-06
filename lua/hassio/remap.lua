955e0b72939a81c239503aeb47be291a8806caf7
OscarClemente <oscar.clemente@gmail.com>
2023-04-06 12:22:01 +0200

    feat: Add change window remaps and fix bufferline

--- lua/hassio/remap.lua
+++ lua/hassio/remap.lua
@@ -1,9 +1,16 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Buffer movements
 vim.keymap.set("n", "H", ':bprevious<CR>')
 vim.keymap.set("n", "L", ':bnext<CR>')
 vim.keymap.set("n", "<leader>q", ':NvimTreeToggle<CR>')
 vim.keymap.set("n", "<leader>c", ':Bdelete!<CR>')
+
+-- Window movements
+vim.keymap.set("n", "<leader>h", '<C-w>h')
+vim.keymap.set("n", "<leader>j", '<C-w>j')
+vim.keymap.set("n", "<leader>k", '<C-w>k')
+vim.keymap.set("n", "<leader>l", '<C-w>l')
 
 -- neotest
 vim.keymap.set("n", "<leader>tn", function()
