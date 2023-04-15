1b42b0b0b763277ce513cb1152850132f84684d8
OscarClemente <oscar.clemente@gmail.com>
2023-04-15 15:32:15 +0200

    feat: Proper usage of lspsaga

--- lua/hassio/packer.lua
+++ lua/hassio/packer.lua
@@ -58,6 +58,21 @@ return require('packer').startup(function(use)
             { 'rafamadriz/friendly-snippets' }, -- Optional
         }
     }
+    use({
+        "glepnir/lspsaga.nvim",
+        opt = true,
+        branch = "main",
+        event = "LspAttach",
+        config = function()
+            require("lspsaga").setup({})
+        end,
+        requires = {
+            { "nvim-tree/nvim-web-devicons" },
+            --Please make sure you install markdown and markdown_inline parser
+            { "nvim-treesitter/nvim-treesitter" }
+        }
+    })
+
     use {
         "nvim-neotest/neotest",
         opt = true,
